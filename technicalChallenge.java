1f22f032879004f3beb75dced180f5d8d1ed1576
jdonnelly234 <jdonnelly234@gmail.com>
2023-11-07 18:47:09 +0000

    Methods for cleaning word and anagram comparison

--- technicalChallenge.java
+++ technicalChallenge.java
@@ -1,10 +1,34 @@
+import java.util.Arrays;
+
+
 public class technicalChallenge{
 
-    public static void main(String[] args)  {
-        int x = 10;
-        int y = 5;
+    //Converts input to lowercase and removes spaces
+    private static String cleanWord(String x)    {
+        return x.replaceAll("\\s", "").toLowerCase();
+    }
 
-        System.out.println(x + y);
+    //Chcks if two inputted words are anagrams
+    public static boolean isAnagram(String firstWord, String secondWord)    {
+
+        firstWord = cleanWord(firstWord);
+        secondWord = cleanWord(secondWord);
+
+        //Checks lengths and if unequal returns false
+        if(firstWord.length() != secondWord.length())   {
+            return false;
+        }
+
+        char[] firstWordChars = firstWord.toCharArray();
+        char[] secondWordChars = secondWord.toCharArray();
+
+        Arrays.sort(firstWordChars);
+        Arrays.sort(secondWordChars);
+
+        return Arrays.equals(firstWordChars, secondWordChars);
+    }
+    public static void main(String[] args)  {
+        
     }
 
 }
